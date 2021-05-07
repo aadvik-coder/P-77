105c1ed54d7a155dc942c0f31a17420dd425a567
aadvik-coder <80146416+aadvik-coder@users.noreply.github.com>
2021-05-07 15:45:43 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log('This is the console screen.')
+var first_name = "Aadvik"
+console.log(first_name);
+var last_name = "Narang"
+console.log(last_name);
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var number1 = 2;
+console.log(number1);
+
+var number_2 = 5;
+console.log(number_2)
+
+var multiply = number1 * number_2;
+console.log(multiply)
+
+var divide = number1 / number_2;
+console.log(divide);
+
+var add = number1 + number_2;
+console.log(add);
+
+var subtract = number1 - number_2;
+console.log(subtract);
